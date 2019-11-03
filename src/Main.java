41baa948aa417656eaf691c8351307c099cf086d
yasinehsan <yasinehsan11@gmail.com>
2019-11-03 10:37:00 -0500

    added treenode

--- src/Main.java
+++ src/Main.java
@@ -6,63 +6,70 @@ import java.util.*;
 
 public class Main {
 
+    // Given K sorted lists of up to N elements in each list, return a sorted iterator over all the items.
 
-//     * Definition for a binary tree node.
-    public class TreeNode {
-         int val;
-         TreeNode left;
-         TreeNode right;
-         TreeNode(int x) { val = x; }
-     }
+        /*
+
+        interface SortedIterator<Int> {
+              constructor(List<List<Int>> input);
+              boolean hasNext();
+              Int next();
+        }
+
+        tuple()
+        [2,1,2]
+
+        1: [X, x, 5, 8, 9]
+        2: [X, 7, 7,10]
+        3: [X, X, 8]
+
+        YourIterator i; ...
+        i.next() // 0
+        i.hasNext() // true
+        i.next() // 0
+        i.next() // 1
+        */
+
+    public class iterator{
+        List<List<Integer>> input;
+        int[] posPointers;
+//        int lastVisited;
 
 
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
+        public iterator(List<List<Integer>> input){
+            this.input = input;
+            posPointers = new int[input.size()];
+            for(int i = 0; i < input.size(); i++){
+                posPointers[i] = input.get(i).get(i);
+            }
+//            lastVisited = 0;
+        }
+
+        public int Next(){
         /*Plan
-            - recurse we need base case if node is null
-            - pre order traversal will be NLR print left right
-            - however we are appending to a string thus we need left and right first
-            - when we return we return like root.val + left + right
-            - we need a way to keep track of sperate chracters like commas
+            - we have k posPointers store the least amount of number in each lsit
+            - we init a variable store we loop pospointer
+            - store stores the number the number closest to lstvisited
+            - update pointerArray if list ends than store -1. update last visited
+            - when we loop again and see -1 we skip
          */
+            int[] store = {posPointers[0], 0};
+            for(int i = 1; i < input.size(); i++){
+                if(posPointers[i] == -1)
+                    continue;
+                if (input.get(i).get(posPointers[i]) < input.get(store[0]).get(posPointers[store[0]])) {
+                    store[0] = input.get(i).get(posPointers[i]);
+                    store[1] = i;
+                }
+            }
 
-        if(root == null)
-            return "NULL,";
+            if(posPointers[store[1]]++ == input.get(store[1]).size())
+                posPointers[store[1]] = -1;
+            return input.get()
 
-        String leftNode = serialize(root.left);
-        String rightNode = serialize(root.right);
-        return root.val + "," + leftNode + rightNode;
-
-    }
-
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        /*Plan
-            - we need to store the values of treenode that we can acesss queue works
-            - so lets store the values of string seprated by comma into queue
-            - we can use a helper function to to the meat of deserilize
-            - the helper fuction takes in a queue if node is "null" returns null
-            - elif creats a new tree node with that val
-            - then calls deserialize on left tree node and right trre node
-            - lastly returns root
-         */
-
-        Queue<String> nodeFile = new LinkedList<>();
-        nodeFile.addAll(Arrays.asList(data.split(",")));
-        return helper(nodeFile);
-    }
-
-    private TreeNode helper(Queue<String> nodeFile){
-        String parsed = nodeFile.poll();
-        if(parsed.equals("NULL"))
-            return null;
-        TreeNode currNode = new TreeNode(Integer.valueOf(parsed));
-        currNode.left = helper(nodeFile);
-        currNode.right = helper(nodeFile);
-        return currNode;
+        }
 
     }
-
 
 
 
